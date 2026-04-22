e92599e0912d061482472a1c7b39b91263fc4860
andervallejolarre <ander.vallejo.larre@gmail.com>
2026-04-22 20:49:18 +0200

    first draft

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,23 @@ import heroImg from './assets/hero.png'
 import List from './components/List.jsx'
 import './App.css'
 
+  /*
+ 1.I'm going to start by defining the formState we are going to use
+   1.1 We're trying to use an array
+ 2.Start defining the static elements of the HTML in App
+   2.1 h1
+   2.2 input
+   2.3 button + p
+   2.4 div, section, etc 
+ 3.first Status functions (onChange, onClick or onSubmit)
+   3.1 Here we have to manage onChange for the input element
+   3.2 Here we have to manage onSubmit with click and Enter to add the toDo or onClick because of the array state
+ 4.Going to List
+ 5.Styling CSS
+   5.1 App: Flex, margins, typo, colors, size
+   5.2 List: flex, spacing, margins, colors, underline
+  */
+
 function App() {
 
   const [listState, setListState] = useState([]);
@@ -17,30 +34,6 @@ function App() {
     setText('');
 
   }
-
-
-  /*
- 1.I'm going to start by defining the formState we are going to use
-   1.1 We're trying to use an array
- 2.Start defining the static elements of the HTML in App
-   2.1 h1
-   2.2 input
-   2.3 button + p
-   2.4 div, section, etc 
- 3.first Status functions (onChange, obSubmit)
-   3.1 Here we have to manage onChange for the input element
-   3.2 Here we have to manage onSubmit with click and Enter to add the toDo
- 4.List html elements
-   4.1 ul element wrapping everything
-   4.2 li elements with checkbox, text and erase icon
- 5. map function to render li elements for every item in the list
- 6.Status functions
-   6.1 onClick (app.jsx?)
-   6.2 onMouse (app.jsx?)
- 7.Styling CSS
-   7.1 App: Flex, margins, typo, colors, size
-   7.2 List: flex, spacing, margins, colors, underline
-  */
 
   return (
     <>
@@ -60,3 +53,4 @@ function App() {
 }
 
 export default App
+
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,3 +1,12 @@
+/*
+1. List html elements
+   1.1 ul element wrapping everything
+   1.2 li elements with checkbox, text and erase icon
+ 2. map function to render li elements for every item in the list
+ 3.Status functions
+   3.1 onClick
+   3.2 onMouse */
+
 const List = (props) => {
 
     let finalList = props.printList.map((item, index) => {
